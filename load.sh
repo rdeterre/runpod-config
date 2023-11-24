e66b2a42df5945ec4405ab52e451a5ca227c17cb
Romain Deterre <romain.deterre@gmail.com>
2023-11-23 22:07:09 -0500

    Fixes

--- load.sh
+++ load.sh
@@ -1,9 +1,6 @@
 
-wget -nv "https://civitai.com/api/download/models/87953?type=Model&format=SafeTensor&size=full&fp=fp32" \ 
-     -O /workplace/stable-diffusion-webui/models/Stable-diffusion/virile_reality_v10.safetensors
-wget -nv "https://civitai.com/api/download/models/181248?type=Model&format=SafeTensor&size=pruned&fp=fp16" \
-     -O /workplace/stable-diffusion-webui/models/Stable-diffusion/virile_reality_v3.safetensors
-wget -nv "https://civitai.com/api/download/models/166323"
-     -O /workspace/stable-diffusion-webui/models/Lora/PrettyBoyV2.safetensors
+wget -nv "https://civitai.com/api/download/models/87953?type=Model&format=SafeTensor&size=full&fp=fp32"    -O /workplace/stable-diffusion-webui/models/Stable-diffusion/virile_reality_v10.safetensors
+wget -nv "https://civitai.com/api/download/models/181248?type=Model&format=SafeTensor&size=pruned&fp=fp16" -O /workplace/stable-diffusion-webui/models/Stable-diffusion/virile_reality_v3.safetensors
+wget -nv "https://civitai.com/api/download/models/166323"                                                  -O /workspace/stable-diffusion-webui/models/Lora/PrettyBoyV2.safetensors
 
 echo "All good girl!"
